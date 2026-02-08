77bff2080c9b2160b3fb0287c767693fbb0e7069
jmatuzak01 <matuzakjosiah@gmail.com>
2026-02-07 22:11:02 -0500

    Add log basic functionality

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("Quitting game.")
