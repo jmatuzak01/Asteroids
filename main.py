f0c7f8186d18e873330ebb259e177eaf853ca03f
jmatuzak01 <matuzakjosiah@gmail.com>
2026-02-07 22:07:24 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    pygame.init()
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Quitting game.")
+                pygame.quit()
+                return
+            screen.fill("black")
+            
+            pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
