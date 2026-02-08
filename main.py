ff60b80a1cc814f43e42d61fd02dc2e5d1ddc479
jmatuzak01 <matuzakjosiah@gmail.com>
2026-02-07 22:55:21 -0500

    Added player icon, player hitbox, player rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH,PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}, Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameClock = pygame.time.Clock()
+    dt: int = 0
+    playerIcon = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,10 +20,11 @@ def main():
                 print("Quitting game.")
                 pygame.quit()
                 return
-            screen.fill("black")
-            
-            pygame.display.flip()
-
+        screen.fill("black")
+        playerIcon.draw(screen)
+        playerIcon.update(dt)
+        pygame.display.flip()
+        dt = gameClock.tick(60) / 1000  # Limit to 60 FPS and convert to seconds
 
 
 if __name__ == "__main__":
